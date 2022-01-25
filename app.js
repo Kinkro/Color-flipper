195e2e482bda2ae8cc84a6a2cfda293cdead0b88
Alexandre <alexkinkriashvili@gmail.com>
2022-01-25 15:20:01 +0400

    Added functionality

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const colors = ["green", "red", "rgba(133,122,200)","#f15025"];
 
+// selecting colors and btn
+const color = document.querySelector('.color');
+const btn = document.getElementById('btn');
+
+// listnening to the btn click
+btn.addEventListener('click', ()=> {
+    let randomNumber = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
